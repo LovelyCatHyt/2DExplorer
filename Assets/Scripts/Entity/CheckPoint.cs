a6112a432670dfa7926ff028bd2413b316fda88b
Hyt <69683382@qq.com>
2022-02-27 12:05:28 +0800

    feat: 检查点有动画变化
    重绘了检查点的贴图, 使其渲染效果更合理
    仅仅色相的变化似乎不是很明显, 可能在真HDR显示器效果会更好(笑)

--- Assets/Scripts/Entity/CheckPoint.cs
+++ Assets/Scripts/Entity/CheckPoint.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace Entity
@@ -12,6 +13,31 @@ namespace Entity
     {
         public MainCharacter connectedChar;
         public int index;
+        public float colorChangeDuration;
+        [ColorUsage(true, true)] public Color initColor;
+        [ColorUsage(true, true)] public Color activatedColor;
+        [ColorUsage(true, true)] public Color disconnectColor;
+
+        private readonly int _emissionProp = Shader.PropertyToID("_Emission");
+        private Material _material;
+
+        private void OnValidate()
+        {
+            var mat = GetComponent<Renderer>().sharedMaterial;
+            mat.SetColor(_emissionProp, initColor);
+        }
+
+        private void Awake()
+        {
+            _material = GetComponent<Renderer>().material;
+            _material.SetColor(_emissionProp, index == 0 ? activatedColor : initColor);
+        }
+
+        private void DisConnect()
+        {
+            connectedChar = null;
+            _material.DOColor(disconnectColor, _emissionProp, colorChangeDuration);
+        }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
@@ -19,13 +45,15 @@ namespace Entity
             if (!character) return;
             if (character.currentCheckPoint.index < index)
             {
+                // 断开上一个检查点的连接, 重定位到 this
+                character.currentCheckPoint.DisConnect();
                 character.currentCheckPoint = this;
                 connectedChar = character;
+                _material.DOColor(activatedColor, _emissionProp, colorChangeDuration);
             }
 
             if (character.currentCheckPoint == this)
             {
-                // Debug.Log($"Character {character} enter the connected checkpoint.");
             }
         }
     }
