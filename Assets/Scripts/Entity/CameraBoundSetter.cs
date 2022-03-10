28595a0fa5a247c827352ebb3ea46c392c3f9b1c
Hyt <69683382@qq.com>
2022-03-10 21:29:25 +0800

    fix: 现在CameraBoundsSetter能正常重载了
    加了两项数据
    修复了 #11 第二点

--- Assets/Scripts/Entity/CameraBoundSetter.cs
+++ Assets/Scripts/Entity/CameraBoundSetter.cs
@@ -10,18 +10,39 @@ namespace Entity
     /// <summary>
     /// 摄像机范围设置器
     /// </summary>
-    [RequireComponent(typeof(Collider2D))]
+    [RequireComponent(typeof(BoxCollider2D))]
     public class CameraBoundSetter : MonoBehaviour, IHasExtraData
     {
         public Bounds2D bounds;
 
         public Dictionary<string, object> ExtraData
         {
-            get => new Dictionary<string, object> {{$"CameraBoundSetter.{nameof(bounds)}", bounds}};
+            get
+            {
+                var box = GetComponent<BoxCollider2D>();
+                JObject boxData = new JObject();
+                boxData["offset"] = JToken.FromObject(box.offset);
+                boxData["size"] = JToken.FromObject(box.size);
+                return new Dictionary<string, object>
+                {
+                    {$"CameraBoundSetter.{nameof(bounds)}", bounds},
+                    {"CameraBoundSetter.BoxCollider2D", boxData}
+                };
+            }
             set
             {
+                if (value.TryGetValue("CameraBoundSetter.BoxCollider2D", out var box))
+                {
+                    if(box is JObject b)
+                    {
+                        var collider = GetComponent<BoxCollider2D>();
+                        collider.offset = b["offset"]?.ToObject<Vector2>() ?? Vector2.zero;
+                        collider.size = b["size"]?.ToObject<Vector2>() ?? Vector2.one;
+                    }
+                }
                 if(value.TryGetValue($"CameraBoundSetter.{nameof(bounds)}", out var obj))
                 {
+                    // TODO: set box collider
                     if (obj is JObject jObj)
                     {
                         bounds = jObj.ToObject<Bounds2D>();
@@ -55,7 +76,6 @@ namespace Entity
         public void SetBoundsAsBoxCollider()
         {
             var b = GetComponent<BoxCollider2D>();
-            if (!b) return;
             bounds.Center = transform.TransformPoint(b.offset);
             bounds.Size = b.size;
         }
